620afa7407a93bc05a568d393f741386c94602e5
Deadlegend1321 <mudit1318@outlook.com>
2023-05-31 19:06:48 +0530

    Added Database Authentication

--- src/main/java/com/mudit/blog/config/SecurityConfig.java
+++ src/main/java/com/mudit/blog/config/SecurityConfig.java
@@ -1,23 +1,28 @@
 package com.mudit.blog.config;
 
+import com.mudit.blog.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder(){
@@ -32,10 +37,24 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated())
                 .httpBasic(Customizer.withDefaults());
+        http.authenticationProvider(daoAuthenticationProvider());
         return http.build();
     }
 
     @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(this.userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    /*@Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails mudit = User.builder()
                 .username("mudit")
@@ -48,5 +67,5 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(mudit, admin);
-    }
+    }*/
 }
---
+++ src/main/java/com/mudit/blog/entity/Role.java
@@ -0,0 +1,19 @@
+package com.mudit.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 60)
+    private String name;
+}
